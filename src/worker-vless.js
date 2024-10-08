c392487108dd275578c06cb5b753caf7fab21ec9
happy-game11 <93173775+happy-drink@users.noreply.github.com>
2024-10-08 09:35:33 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '752f056f-2553-4c3b-9a52-170c9fc50cd8';
 
 let proxyIP = '';
 
